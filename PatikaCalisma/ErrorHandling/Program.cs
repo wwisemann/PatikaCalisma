87c171226bb00f8656f38dd5b4fa03600c8ec721
Arifcan Yılmaz <83744320+wwisemann@users.noreply.github.com>
2023-07-30 00:00:14 +0300

    Add files via upload

---
+++ PatikaCalisma/ErrorHandling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ErrorHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayi giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            {
+
+                Console.WriteLine("İşlem tamam bro.");
+            }
+
+            try
+            {
+                //int a = int.Parse(null);
+                int a = int.Parse("test");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz! ");
+                Console.WriteLine(ex.Message);  
+            }
+            catch (FormatException ex) 
+            {
+                Console.WriteLine("Format uyumsuzluğu!");
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}

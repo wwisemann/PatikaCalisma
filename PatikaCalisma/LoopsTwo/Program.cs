0ad52d09d15793fdd0da5b774cb16f79dd65ad2f
Arifcan Yılmaz <83744320+wwisemann@users.noreply.github.com>
2023-07-30 13:43:33 +0300

    Add files via upload

---
+++ PatikaCalisma/LoopsTwo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LoopsTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // While
+            // 1'den başlayarak console'dan girilen sayıya kadar (sayi dahil) ortalama hesaplayıp console yazdıran program.
+            Console.WriteLine("Lütfen bir sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            // a'dan z'ye kadar olan tüm harleri console'a yazdır.
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+            Console.WriteLine("\n*****Foreach*****");
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}

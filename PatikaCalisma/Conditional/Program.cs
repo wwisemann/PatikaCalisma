ba1f6be0db829b047276b16db31a8e262c7fbc10
Arifcan Yılmaz <83744320+wwisemann@users.noreply.github.com>
2023-07-30 11:35:30 +0300

    Add files via upload

---
+++ PatikaCalisma/Conditional/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conditional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time <= 18) 
+            {
+                Console.WriteLine("Gündüz vakti.");
+            }else if (time >=4 && time <= 5)
+            {
+                Console.WriteLine("Namaz vakti");
+            }
+            else
+            {
+                Console.WriteLine("Gece vakti.");
+            }
+        }
+    }
+}

979196862cca1c81af882ab3d115c5f15f303f53
Arifcan Yılmaz <83744320+wwisemann@users.noreply.github.com>
2023-08-16 10:36:20 +0300

    Add files via upload

---
+++ PatikaCalisma/Enum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+
+            if (sicaklik <= (int)Hava_Durumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyelim");
+            else if (sicaklik >= (int)Hava_Durumu.Sicak)
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            else if (sicaklik >= (int)Hava_Durumu.Normal && sicaklik <= (int)Hava_Durumu.Cok_Sicak)
+                Console.WriteLine("Hadi dışarıya çıkalım");
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma =23,
+        Cumartesi,
+        Pazar
+    }
+    enum Hava_Durumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        Cok_Sicak = 30
+    }
+}
